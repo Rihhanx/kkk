99e34b9ecb3ee3a1b460efeb47ec5ef71e0a318a
Rihhanx <73778977+Rihhanx@users.noreply.github.com>
2021-06-11 22:40:20 +0800

    Add files via upload

--- 源.c
+++ 源.c
@@ -1,22 +1,14 @@
 #include<stdio.h>//头文件
-void Move(int n, char A, char B, char C)//定义函数
-{
-	if (n == 1)
-	{
-		printf("%c-->%c\n", A, C);//输出移动结果
-	}
-	else
-	{
-		Move(n - 1, A, C, B);//把A上n-1个盘子通过借助辅助塔(C塔)移到了B上
-		printf("%c-->%c\n", A, C);//把最大的一个盘子由A移到C上去
-		Move(n - 1, B, A, C); //把B上n-1个盘子通过借助辅助塔(A塔)移到了C上
-	}
-}
 int main()//主函数
 {
-	int n = 0;//定义字符变量
-	printf("请输入汉诺塔的盘子数:");//提示语句
-	scanf_s("%d", &n);//键盘输入
-	Move(n, 'A', 'B', 'C');
-	return 0;//函数返回值为0
+	char ch;//定义字符变量
+	printf("请输入一个字符:");//提示语句
+	scanf_s("%c",&ch);//键盘输入
+	if (ch >='a'&& ch <='z'|| ch >='A'&& ch <='Z')//字母的取值范围
+		printf("%c是一个字母\n", ch);//输出字符ch的类别
+	else if (ch >='0'&& ch <='9')//数字的取值范围
+		printf("%c是一个数字\n", ch);//输出字符ch的类别
+	else
+		printf("%c是一个特殊字符\n", ch);//输出字符ch的类别
+	return 0;////函数返回值为0 
 }
